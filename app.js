d4f9f7c1f0169fd97cc3de54fa381dfe6ba2a2c2
Asif Asif <130058986+asifhameed762@users.noreply.github.com>
2024-03-04 19:55:18 +0500

    chapter 2

---
+++ app.js
@@ -0,0 +1,46 @@
+// chapter 2
+// Q1.
+var username;
+console.log(username);
+
+// Q2.
+var myname ;
+myname = "Muhammad Asif";
+console.log(myname);
+
+// Q3.
+var message;
+message = "Hello World";
+alert(message);
+
+//Q4.
+var studentname = "Asif";
+var studentage = "30";
+var studentsubject = "Web And Mobile App Development";
+
+alert(studentname);
+alert(studentage);
+alert(studentsubject);
+
+//Q5.
+var dish = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(dish);
+
+//Q6.
+var email;
+email = "asifhameed762@gmail.com";
+alert("My mail address is " +  email);
+
+//Q7:
+var book;
+book = "A smarter\nway to learn JavaScript";
+alert("I am trying to learn from the Book " + book)
+
+//Q8.
+document.write("Q8. Yah! I can write HTML content through JavaScript");
+
+//Q9.
+var diagram;
+diagram = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(diagram);
+document.write("</br> Q9. " + diagram);
